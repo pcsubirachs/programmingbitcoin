2e5db880b050a7bf71d4b3c668166d801b2a047c
Subirachs <pcsubirachs@protonmail.com>
2021-06-17 19:57:42 -0700

    completed more exercises

--- code-ch01/ecc.py
+++ code-ch01/ecc.py
@@ -39,7 +39,8 @@ class FieldElement:
         # self.num and other.num are the actual values
         # self.prime is what we need to mod against
         # We return an element of the same class
-        raise NotImplementedError
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
 
     def __mul__(self, other):
         if self.prime != other.prime:
